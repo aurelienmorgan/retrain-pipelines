aurelienmorgan/retrain-pipelines

commit 818e26430f007be32fd2169ae591c3ee7547c05e

Implemented tasks instances collection logic
for the Gantt diagram of the execution-details page
of the internal DAG engine's WebConsole

and started developing the 'exec_current_progress'
async endpoint, which will be responsible for
Gantt diagram at component load time.
Comes together with the 'draw_chart' method,
the drafting has also been initiated.

Incidentally,
  - changed the DAO below 2 methods
    (both related to DAG rendering)
    so they now return lists of
    model object instances and
    not serializable dicts anymore :
      - get_execution_tasktypes_list
      - get_execution_taskgroups_list
  - created the TaskExt class, to allow
    for task & tasktype attributes
    to be handled conjointly.
  - renamed 'get_execution_elements_lists'
    into 'get_execution_dag_elements_lists'
    so Gantt-related and DAG-related methods
    have clear, distinct names.

remains to :
  - address the wrong ordering of parallel tasks,
    need to sort by (potentially nested) rank.

pkg_src/retrain_pipelines/dag_engine/web_console/utils/execution/gantt_chart.py
@@ -1,0 +1,197 @@
+
+from typing import List, Tuple, Union
+
+from fasthtml.common import Table, Tr, Td
+
+from ....db.model import TaskExt, TaskGroup
+
+
+def _organize_tasks(
+    tasks_list: List[TaskExt],
+    taskgroups_list: List[TaskGroup]
+) -> List[Union[TaskExt, Tuple]]:
+    """Returns a topologically-organized structure
+
+    e.g.:
+    ```python
+    tasks_list = [
+        Task("task1", ""),
+        Task("task2", "tg1"),
+        Task("task3", "tg1"),
+        Task("task4", "tg2"),
+        Task("task5", "tg2"),
+        Task("task6"),
+        Task("task7", "tg3"),
+        Task("task8", "tg3"),
+        Task("task9")
+    ]
+    taskgroups_list = [
+        TaskGroup("tg1", ["task2", "task3", "tg2"]),
+        TaskGroup("tg2", ["task4", "task5"]),
+        TaskGroup("tg3", ["task7", "task8"])
+    ]
+    organize_tasks(tasks_list, taskgroups_list)
+    # returns
+    # ['task1', ('tg1', ['task2', 'task3',
+    #                    ('tg2', ['task4', 'task5'])]),
+    #  'task6', ('tg3', ['task7', 'task8']), 'task9']
+    ```
+
+    Params:
+        - tasks_list (List[TaskExt]):
+            the exhaustive (topologically sorted)
+            list of tasks for a given execution.
+        - taskgroups_list (List[TaskGroup]):
+            the exhaustive (topologically sorted)
+            list of taskgroups for that execution.
+
+    Results:
+        - (List[Union[TaskExt, Tuple]]):
+            a nested list of
+            "task" and "tuple(taskgroup, tasks)".
+    """
+    task_by_id = {task_ext.id: task_ext for task_ext in tasks_list}
+    taskgroup_by_uuid = {str(tg.uuid): tg for tg in taskgroups_list} \
+                        if taskgroups_list else {}
+
+    children_map = {}
+    parent_of_taskgroup = {}
+    if taskgroups_list:
+        # Map each taskgroup to its direct children (tasks and taskgroups)
+        for tg in taskgroups_list:
+            for tg_element_uuid in tg.elements:
+                # check if element is a task and, if so, add its id
+                for task_ext in tasks_list:
+                    if str(task_ext.tasktype_uuid) == tg_element_uuid:
+                        children_map[str(tg.uuid)] = [task_ext.id] + \
+                            ([] if not str(tg.uuid) in children_map
+                             else children_map[str(tg.uuid)])
+                        # DO NOT BREAK, parallel tasks have different ids
+                        # but common tasktype uuid
+
+                children_map[str(tg.uuid)] = tg.elements + \
+                    ([] if not str(tg.uuid) in children_map
+                     else children_map[str(tg.uuid)])
+
+        # Determine which taskgroup (if any) contains each taskgroup
+        for tg in taskgroups_list:
+            for element in tg.elements:
+                if element in taskgroup_by_uuid:
+                    parent_of_taskgroup[element] = str(tg.uuid)
+
+    emitted = set()
+    result = []
+
+    def emit_taskgroup(tg_uuid):
+        emitted.add(tg_uuid)
+        children = []
+        for element in children_map[tg_uuid]:
+            if element in task_by_id:
+                if element not in emitted:
+                    emitted.add(element)
+                    children.append(task_by_id[element])
+            elif element in taskgroup_by_uuid:
+                if element not in emitted:
+                    children.append(emit_taskgroup(element))
+        return (taskgroup_by_uuid[tg_uuid], children)
+
+    for task_ext in tasks_list:
+        if task_ext.id in emitted:
+            continue
+        if task_ext.taskgroup_uuid is None or task_ext.taskgroup_uuid == "":
+            result.append(task_ext)
+            emitted.add(task_ext.id)
+        else:
+            tg_uuid = str(task_ext.taskgroup_uuid)
+            # Find the top-most taskgroup that contains this task and isn't emitted
+            current = tg_uuid
+            while current in parent_of_taskgroup:
+                parent_uuid = parent_of_taskgroup[current]
+                if parent_uuid in emitted:
+                    break
+                current = parent_uuid
+            if current not in emitted:
+                tg_struct = emit_taskgroup(current)
+                result.append(tg_struct)
+
+    # print("organize_tasks result :\n", result)
+    return result
+
+
+def draw_chart(
+    tasks_list: List[TaskExt],
+    taskgroups_list: List[TaskGroup]
+) -> Table:
+    """The html DOM element
+
+    for the Gantt diagramm of a given execution.
+
+    Params:
+        - tasks_list (List[TaskExt]):
+            the exhaustive (topologically sorted)
+            list of tasks for a given execution.
+        - taskgroups_list (List[TaskGroup]):
+            the exhaustive (topologically sorted)
+            list of taskgroups for that execution.
+
+    Results:
+        - (Table)
+    """
+
+    current_organize_tasks = _organize_tasks(tasks_list, taskgroups_list)
+
+    rows = []
+    for element in current_organize_tasks:
+        if isinstance(element, Tuple):
+            rows.append(taskgroup_table(element))
+        else:
+            rows.append(task_row(element))
+
+    return Table(*rows)
+
+
+def task_row(task_ext: TaskExt) -> Tr:
+    result = Tr(
+        Td(
+            task_ext.name
+        ),
+        id=task_ext.id
+    )
+
+    return result
+
+
+def taskgroup_table(
+    taskgroup_tuple: Tuple[TaskGroup, List[Union[TaskExt, Tuple]]]
+) -> Tr:
+    taskgroup, taskgroup_elements = taskgroup_tuple
+
+    rows = []
+    for element in taskgroup_elements:
+        if isinstance(element, Tuple):
+            rows.append(taskgroup_table(element))
+        else:
+            rows.append(task_row(element))
+
+    return Tr(Td(Table(
+            (taskgroup.name, *rows),
+            id=taskgroup.uuid
+        )))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

pkg_src/retrain_pipelines/dag_engine/db/dao.py
@@ -17,7 +17,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, \
     AsyncSession
 
 from .model import Base, Execution, ExecutionExt, \
-    TaskType, Task, TaskGroup
+    TaskType, Task, TaskExt, TaskGroup
 
 logger = logging.getLogger()
 
@@ -325,21 +325,77 @@ class AsyncDAO(DAOBase):
 
             return executions_ext
 
-    @lru_cache
-    async def get_execution_tasktypes_list(
-        self, execution_id: int,
-        serializable: bool = False
-    ) -> Optional[List[dict]]:
+    async def get_execution_tasks_list(
+        self, execution_id: int
+    ) -> Optional[List[TaskExt]]:
         """
-        Return all TaskType rows for the given execution_id
-        as list of dicts.
+        Return all Task records for the given execution_id
+        as list.
+
+        Note: no cache, tasks are living vars.
 
         Params:
             - execution_id (int):
                 The Execution.id to filter TaskType items on.
-            - serializable (bool):
-                If True, UUIDs are converted to strings
-                for JSON-safe output.
+
+        Results:
+            list of tasks if found, else None
+        """
+        tasktype_subq = (
+            select(
+                TaskType.uuid,
+                TaskType.order,
+                TaskType.name,
+                TaskType.ui_css,
+                TaskType.taskgroup_uuid
+            )
+            .where(TaskType.exec_id == execution_id)
+            .subquery()
+        )
+
+        main_stmt = (
+            select(
+                tasktype_subq.c.name,
+                tasktype_subq.c.ui_css,
+                tasktype_subq.c.taskgroup_uuid,
+
+                Task
+            )
+            .where(Task.tasktype_uuid == tasktype_subq.c.uuid)
+            .order_by(tasktype_subq.c.order, Task._start_timestamp)
+        )
+
+        async with self._get_session() as session:
+            result = await session.execute(main_stmt)
+            rows = result.all()
+
+            if not rows:
+                return None
+
+            # Convert ORM objects to model objects
+            out_list = []
+            for name, ui_css, taskgroup_uuid, task_orm_obj in rows:
+                task_ext = TaskExt(**{**task_orm_obj.__dict__,
+                                      "name": name, "ui_css": ui_css,
+                                      "taskgroup_uuid": taskgroup_uuid})
+                out_list.append(task_ext)
+
+            return out_list
+
+    @lru_cache
+    async def get_execution_tasktypes_list(
+        self, execution_id: int
+    ) -> Optional[List[TaskType]]:
+        """
+        Return all TaskType rows for the given execution_id
+        as list.
+
+        Params:
+            - execution_id (int):
+                The Execution.id to filter TaskType items on.
+
+        Results:
+            list of tasktypes if found, else None
         """
         statement = (
             select(TaskType)
@@ -354,34 +410,27 @@ class AsyncDAO(DAOBase):
             if not rows:
                 return None
 
-            # Convert ORM objects to pure dictionaries
+            # Convert ORM objects to model object
             out_list = []
-            for row in rows:
-                row_dict = {}
-                for col in TaskType.__table__.columns:
-                    val = getattr(row, col.name)
-                    if serializable and isinstance(val, UUID):
-                        val = str(val)
-                    row_dict[col.name] = val
-                out_list.append(row_dict)
+            for tasktype_orm_obj in rows:
+                out_list.append(TaskType(**tasktype_orm_obj.__dict__))
 
             return out_list
 
     @lru_cache
     async def get_execution_taskgroups_list(
-        self, execution_id: int,
-        serializable: bool = False
-    ) -> Optional[List[dict]]:
+        self, execution_id: int
+    ) -> Optional[List[TaskGroup]]:
         """
-        Return all TaskGroup rows for the given execution_id
-        as list of dicts.
+        Return all TaskGroup records for the given execution_id
+        as list.
 
         Params:
             - execution_id (int):
                 The Execution.id to filter TaskGroup items on.
-            - serializable (bool):
-                If True, UUIDs are converted to strings
-                for JSON-safe output.
+
+        Results:
+            list of taskgroups if found, else None
         """
         statement = (
             select(TaskGroup)
@@ -396,16 +445,10 @@ class AsyncDAO(DAOBase):
             if not rows:
                 return None
 
-            # Convert ORM objects to pure dictionaries
+            # Convert ORM objects to model object
             out_list = []
-            for row in rows:
-                row_dict = {}
-                for col in TaskGroup.__table__.columns:
-                    val = getattr(row, col.name)
-                    if serializable and isinstance(val, UUID):
-                        val = str(val)
-                    row_dict[col.name] = val
-                out_list.append(row_dict)
+            for taskgroup_orm_obj in rows:
+                out_list.append(TaskGroup(**taskgroup_orm_obj.__dict__))
 
             return out_list
 
@@ -420,7 +463,7 @@ class AsyncDAO(DAOBase):
             - execution_id (int):
                 the Execution.id to query.
 
-        Returns:
+        Results:
             dict with keys 'name' and '_start_timestamp' if found,
             else None
         """
@@ -467,49 +510,49 @@ class AsyncDAO(DAOBase):
               - failed: count of completed executions that
                 have at least one failed Task
         """
+        exec_subq = (
+            select(
+                Execution.name,
+                Execution._start_timestamp.label("start_ts")
+            )
+            .where(Execution.id == execution_id)
+            .subquery()
+        )
+
+        task_alias = aliased(Task)
+        # Subquery condition: completed executions have a task failed
+        failed_exists = (
+            select(1)
+            .where(task_alias.exec_id == Execution.id)
+            .where(task_alias.failed == True)
+            .limit(1)
+            .exists()
+        )
+
+        main_stmt = (
+            select(
+                Execution.name,
+                func.count(Execution.id).label("total_count"),
+                func.count(
+                    func.nullif(
+                        Execution._start_timestamp > exec_subq.c.start_ts, True
+                    )
+                ).label("number"),
+                func.count(
+                    func.nullif(Execution._end_timestamp == None, True)
+                ).label("completed"),
+                func.count(
+                    func.nullif(
+                        (Execution._end_timestamp != None) & failed_exists,
+                        False
+                    )
+                ).label("failed"),
+            )
+            .where(Execution.name == exec_subq.c.name)
+            .group_by(Execution.name)
+        )
+
         async with self._get_session() as session:
-            exec_subq = (
-                select(
-                    Execution.name.label("name"), 
-                    Execution._start_timestamp.label("start_ts")
-                )
-                .where(Execution.id == execution_id)
-                .subquery()
-            )
-
-            task_alias = aliased(Task)
-            # Subquery condition: completed executions have a task failed
-            failed_exists = (
-                select(1)
-                .where(task_alias.exec_id == Execution.id)
-                .where(task_alias.failed == True)
-                .limit(1)
-                .exists()
-            )
-
-            main_stmt = (
-                select(
-                    Execution.name,
-                    func.count(Execution.id).label("total_count"),
-                    func.count(
-                        func.nullif(
-                            Execution._start_timestamp > exec_subq.c.start_ts, True
-                        )
-                    ).label("number"),
-                    func.count(
-                        func.nullif(Execution._end_timestamp == None, True)
-                    ).label("completed"),
-                    func.count(
-                        func.nullif(
-                            (Execution._end_timestamp != None) & failed_exists,
-                            False
-                        )
-                    ).label("failed"),
-                )
-                .where(Execution.name == exec_subq.c.name)
-                .group_by(Execution.name)
-            )
-
             result = await session.execute(main_stmt)
             row = result.first()
             if row is None or row.total_count == 0:

pkg_src/retrain_pipelines/dag_engine/db/model.py
@@ -104,13 +104,11 @@ class ExecutionExt(Execution):
     """Execution class plus failure (computed) attribute."""
     """ NOT AN SQLALCHEMY CLASS """
     __mapper_args__ = {
-        'polymorphic_identity': 'execution_ext'
+        "polymorphic_identity": "task_ext"
     }
 
     def __init__(self, **kwargs):
-        success = kwargs.pop('success', None)
-        # Remove SQLAlchemy internal attributes
-        kwargs.pop('_sa_instance_state', None)
+        success = kwargs.pop("success", None)
 
         # Add underscore prefix for attributes with names
         # that are properties (getters) in parent class
@@ -131,6 +129,9 @@ class ExecutionExt(Execution):
                         ):
                             value = parse_datetime(value)
                 kwargs[f'_{attr_name}'] = value
+
+        # Remove SQLAlchemy internal attributes
+        kwargs.pop('_sa_instance_state', None)
 
         super().__init__(**kwargs)
         self.success = success
@@ -216,6 +217,29 @@ class TaskType(Base):
     taskgroup_uuid = Column(Uuid, nullable=True)
     children = Column(JSON, nullable=False)  # ARRAY(str(Uuid))
 
+    def __init__(self, *args, **kwargs):
+        # Support dict as the ONLY positional argument
+        data = None
+        if len(args) == 1 and isinstance(args[0], dict):
+            data = args[0]
+        elif len(args) > 0:
+            raise TypeError(
+                "TaskType accepts a dict or keyword arguments")
+
+        if data:
+            kwargs = {**data, **kwargs}
+            kwargs["exec_id"] = int(kwargs["exec_id"])
+            kwargs["order"] = int(kwargs["order"])
+            kwargs["docstring"] = \
+                str(kwargs["docstring"]) if "docstring" in kwargs \
+                else None
+            kwargs["is_parallel"] = bool(kwargs["is_parallel"])
+
+        # Remove SQLAlchemy internal attributes
+        kwargs.pop('_sa_instance_state', None)
+
+        super().__init__(**kwargs)
+
 
 class Task(Base):
     """Individual instances of tasktypes,
@@ -272,6 +296,37 @@ class Task(Base):
         ),
     )
 
+    def __init__(self, *args, **kwargs):
+        # Support dict as the ONLY positional argument
+        data = None
+        if len(args) == 1 and isinstance(args[0], dict):
+            data = args[0]
+        elif len(args) > 0:
+            raise TypeError(
+                "Task accepts a dict or keyword arguments")
+
+        if data:
+            kwargs = {**data, **kwargs}
+            kwargs["id"] = int(kwargs["id"])
+            kwargs["docstring"] = \
+                str(kwargs["docstring"]) if "docstring" in kwargs \
+                else None
+            kwargs["_start_timestamp"] = \
+                parse_datetime(kwargs["start_timestamp"])
+            kwargs["_end_timestamp"] = (
+                parse_datetime(kwargs.get("end_timestamp"))
+                if (
+                    "end_timestamp" in kwargs and
+                    kwargs.get("end_timestamp") is not None
+                ) else None
+            )
+            kwargs["failed"] = bool(kwargs["failed"])
+
+        # Remove SQLAlchemy internal attributes
+        kwargs.pop('_sa_instance_state', None)
+
+        super().__init__(**kwargs)
+
     @property
     def start_timestamp(self) -> datetime:
         if self._start_timestamp is None:
@@ -295,6 +350,29 @@ class Task(Base):
     @end_timestamp.setter
     def end_timestamp(self, value: datetime):
         self._end_timestamp = value
+
+
+class TaskExt(Task):
+    """Task class plus attributes from tasktype
+
+    name, taskgroup_uuid and ui_csss.
+    """
+    """ NOT AN SQLALCHEMY CLASS """
+    __mapper_args__ = {
+        "polymorphic_identity": "task_ext"
+    }
+
+    def __init__(self, **kwargs):
+        name = kwargs.pop("name", None)
+        ui_css = kwargs.pop("ui_css", None)
+        taskgroup_uuid = kwargs.pop("taskgroup_uuid", None)
+        # Remove SQLAlchemy internal attributes
+        kwargs.pop('_sa_instance_state', None)
+
+        super().__init__(**kwargs)
+        self.name = name
+        self.ui_css = ui_css
+        self.taskgroup_uuid = taskgroup_uuid
 
 
 class TaskGroup(Base):
@@ -323,4 +401,26 @@ class TaskGroup(Base):
     ui_css = Column(JSON, nullable=True)
 
     elements = Column(JSON, nullable=False)  # ARRAY(str(Uuid))
+
+    def __init__(self, *args, **kwargs):
+        # Support dict as the ONLY positional argument
+        data = None
+        if len(args) == 1 and isinstance(args[0], dict):
+            data = args[0]
+        elif len(args) > 0:
+            raise TypeError(
+                "TaskGroup accepts a dict or keyword arguments")
+
+        if data:
+            kwargs = {**data, **kwargs}
+            kwargs["exec_id"] = int(kwargs["exec_id"])
+            kwargs["order"] = int(kwargs["order"])
+            kwargs["docstring"] = \
+                str(kwargs["docstring"]) if "docstring" in kwargs \
+                else None
+
+        # Remove SQLAlchemy internal attributes
+        kwargs.pop('_sa_instance_state', None)
+
+        super().__init__(**kwargs)
 

pkg_src/retrain_pipelines/dag_engine/web_console/views/execution.py
@@ -2,25 +2,30 @@
 import os
 import asyncio
 
-from typing import Optional, List
+from typing import List, Tuple
 from fasthtml.common import H1, H2, Div, P, \
     Link, Script, Style, \
     Request, Response, JSONResponse, \
     StreamingResponse
 from jinja2 import Environment, FileSystemLoader
 
+
 from ...db.dao import AsyncDAO
+from ...db.model import TaskExt, TaskGroup
+
 from ..utils import ClientInfo
-from .page_template import page_layout
-from ....utils import get_text_pixel_width
 from ..utils.execution.events import \
     new_exec_subscribers, exec_end_subscribers, \
     multiplexed_event_generator, execution_number
+from ..utils.execution.gantt_chart import draw_chart
+
+from .page_template import page_layout
+from ....utils import get_text_pixel_width
 
 
-async def get_execution_elements_lists(
+async def get_execution_dag_elements_lists(
     execution_id: int
-) -> Optional[List[str]]:
+) -> Tuple[List[dict], List[dict]]:
     """Tuple of topologically-sorted serializable lists
 
     of constituting elements, respectively :
@@ -41,13 +46,62 @@ async def get_execution_elements_lists(
     )
 
     execution_tasktypes_list, execution_taskgroups_list = await asyncio.gather(
-        dao.get_execution_tasktypes_list(execution_id, serializable=True),
-        dao.get_execution_taskgroups_list(execution_id, serializable=True)
+        dao.get_execution_tasktypes_list(execution_id),
+        dao.get_execution_taskgroups_list(execution_id)
     )
     # print(f"execution_tasktypes_list : {execution_tasktypes_list}")
     # print(f"execution_taskgroups_list : {execution_taskgroups_list}")
+    if not execution_tasktypes_list:
+        return None, None
+
+
+    # turn into serializables
+    execution_tasktypes_list = \
+        [tasktypes.__dict__ for tasktypes in execution_tasktypes_list]
+    for tasktype_dict in execution_tasktypes_list:
+        tasktype_dict["uuid"] = str(tasktype_dict["uuid"])
+        tasktype_dict["taskgroup_uuid"] = str(tasktype_dict["taskgroup_uuid"]) \
+                                          if tasktype_dict["taskgroup_uuid"] else ""
+
+    execution_taskgroups_list = \
+        [taskgroup.__dict__ for taskgroup in execution_taskgroups_list] \
+        if execution_taskgroups_list else []
+    for taskgroup_dict in execution_taskgroups_list:
+        taskgroup_dict["uuid"] = str(taskgroup_dict["uuid"])
 
     return execution_tasktypes_list, execution_taskgroups_list
+
+
+async def get_execution_elements_lists(
+    execution_id: int
+) -> Tuple[List[TaskExt], List[TaskGroup]]:
+    """Tuple of topologically-sorted lists of model objects.
+
+    of constituting elements, respectively :
+        - all Tasks (exhaustively)
+        - and all TaskGroups.
+
+    Can be None, e.g. if no execution with that id exists.
+
+    Params:
+        - execution_id (int)
+
+    Results:
+        - (List[TaskExt])
+        - (List[TaskGroup])
+    """
+    dao = AsyncDAO(
+        db_url=os.environ["RP_METADATASTORE_ASYNC_URL"]
+    )
+
+    execution_tasks_list, execution_taskgroups_list = await asyncio.gather(
+        dao.get_execution_tasks_list(execution_id),
+        dao.get_execution_taskgroups_list(execution_id)
+    )
+    # print(f"execution_tasks_list : {execution_tasks_list}")
+    # print(f"execution_taskgroups_list : {execution_taskgroups_list}")
+
+    return execution_tasks_list, execution_taskgroups_list
 
 
 def register(app, rt, prefix=""):
@@ -60,7 +114,7 @@ def register(app, rt, prefix=""):
             return Div(P(f"Invalid execution ID {execution_id}"))
 
         tasktypes_list, taskgroups_list = \
-            await get_execution_elements_lists(execution_id)
+            await get_execution_dag_elements_lists(execution_id)
         if tasktypes_list is None:
             return Div(P(f"Invalid execution ID {execution_id}"))
 
@@ -76,6 +130,26 @@ def register(app, rt, prefix=""):
         )
 
         return rendering_content
+
+    @rt(f"{prefix}/exec_current_progress", methods=["GET"])
+    async def exec_current_progress(request: Request):
+        """Progress of the execution.
+
+        May be the final one if the execution is
+        already completed.
+        """
+        execution_id = request.query_params.get("id")
+        try:
+            execution_id = int(execution_id)
+        except (TypeError, ValueError):
+            return Div(P(f"Invalid execution ID {execution_id}"))
+
+        tasks_list, taskgroups_list = \
+            await get_execution_elements_lists(execution_id)
+        if tasks_list is None:
+            return Div(P(f"Invalid execution ID {execution_id}"))
+
+        return draw_chart(tasks_list, taskgroups_list)
 
 
     @rt(f"{prefix}/execution_info", methods=["GET"])
@@ -171,7 +245,7 @@ def register(app, rt, prefix=""):
                     ),
                     id="subtitle"
                 ),
-                Script(f"""// async get execution-info
+                Script(f"""// async get execution-info (name, etc.)
                     function updateExecutionNumber(executionNumberJson) {{
                         // Update the executions counters (incl. tooltip)
                         //console.log("updateExecutionNumber ENTER", executionNumberJson);
@@ -485,7 +559,13 @@ def register(app, rt, prefix=""):
                     }
                 """),
 
-                ## TODO, add stuff here (live-streamed Grantt diagram of tasks, etc.)
+                ## TODO, add stuff here (live-streamed Gantt diagram of tasks, etc.)
+                Div(# Gantt diagram
+                    P("\u00A0 Loading Gantt diagram...", style="color: white;"),
+                    hx_get=f"{prefix}/exec_current_progress?id={execution_id}",
+                    hx_trigger="load",
+                    hx_swap="outerHTML"
+                ),
 
                 H1(
                     "Execution DAG",

commit a093d048c797b730de5a74505b5b3fe8a3a44957

bug fix - taskgroup dragging broke with 818e264
for the execution-details page
of the internal DAG engine's WebConsole DAG renderer.

HTML DOM element IDs non-uniqueness issue,
fixed by prefixing Gantt diagram elements.

pkg_src/retrain_pipelines/dag_engine/web_console/utils/execution/gantt_chart.py
@@ -257,7 +257,7 @@ def task_row(task_ext: TaskExt) -> Tr:
         Td(
             task_ext.name
         ),
-        id=task_ext.id
+        id=f"gantt-{task_ext.id}"
     )
 
     return result
@@ -291,7 +291,7 @@ def parallel_table(
     return Tr(Td(Table(
             *rows,
             style="border: 2px solid #333; border-collapse: collapse;",
-            id=parallel_lines.rank
+            id=f"gantt-{parallel_lines.rank}"
         )))
 
 
@@ -310,7 +310,7 @@ def taskgroup_table(
     return Tr(Td(Table(
             (Tr(Td(B(taskgroup.name))), *rows),
             style="border: 2px solid #FFD700; border-collapse: collapse; ",
-            id=taskgroup.uuid
+            id=f"gantt-{taskgroup.uuid}"
         )))
 
 